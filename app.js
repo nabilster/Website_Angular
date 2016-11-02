3eb8ff4de6ce552c4dd4adae8537f3e286e6d330
Nabil Sakran <sakran@mcmaster.ca>
2016-11-02 15:29:09 -0500

    setup ui.routing in app.js and state for home. Created .run for redirecting

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-var myApp = angular.module('NabilsWebsite',[]);
+var myApp = angular.module('NabilsWebsite',['ui.router']);
+
+angular.module('NabilsWebsite')
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      // Add your own states
+      .state('home', {
+        url: '/home',
+        templateUrl: 'template-home.html',
+      })
+      $urlRouterProvider.otherwise('/home');
+  }])
+
+  .run(['$rootScope', '$state', function($rootScope, $state) {
+      $rootScope.$on('$stateChangeStart', function(evt, to, params) {          
+          if (to.redirectTo) {
+            evt.preventDefault();
+            $state.go(to.redirectTo, params, {location: 'replace'})
+          }
+      })
+  }])
+  
